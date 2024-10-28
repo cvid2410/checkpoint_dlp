c440da927130efff22e6d2f17d13011c1d95bd86
Carlos E Vides <34895364+cvid2410@users.noreply.github.com>
2024-10-28 11:35:26 -0400

    created Pattern and CaughtMessage tables

--- dlp/admin.py
+++ dlp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CaughtMessage, Pattern
+
+
+@admin.register(Pattern)
+class PatternAdmin(admin.ModelAdmin):
+    list_display = ("name", "regex_pattern", "description", "created_at", "updated_at")
+    search_fields = ("name", "regex_pattern")
+
+
+@admin.register(CaughtMessage)
+class CaughtMessageAdmin(admin.ModelAdmin):
+    list_display = ("message_content", "pattern_matched", "timestamp")
+    search_fields = ("message_content",)
+    list_filter = ("pattern_matched", "timestamp")
--- dlp/models.py
+++ dlp/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    'created_at' and 'updated_at' fields and logs attribute setting errors.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Pattern(BaseModel):
+    name = models.CharField(max_length=100)
+    regex_pattern = models.CharField(max_length=500)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CaughtMessage(BaseModel):
+    message_content = models.TextField()
+    pattern_matched = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    additional_info = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Message caught at {self.created_at}"
